a9de73790f950540904ae26100d0d3f49a8fbd28
nicholas <nicholas@moonjoy.net>
2018-03-13 13:36:30 +0700

    remove predis

--- routes/pc.php
+++ routes/pc.php
@@ -2,16 +2,14 @@
 /**
  * PC端路由
  */
-//Route::get('/user', 'UserController@index');
-//$app->addRoute(['POST', 'GET'], '/login', ['as' => 'login', 'uses' => 'App\Controller\LoginController@login']);
+
 Route::get('/', function () {
-    $a = app('request')->getSession();
-    dd($a);
+//    $a = app('request')->getSession();
     echo 'pc index';
 });
 
-Route::group(['middleware'=>['login_auth']],function (){
-    Route::match(['POST', 'GET'], '/onetomore', ['name' => 'login', 'uses' => 'LoginController@login']);
-});
-Route::match(['POST', 'GET'], '/login', ['name' => 'login', 'uses' => 'LoginController@login']);
+//Route::group(['middleware'=>['login_auth']],function (){
+//    Route::match(['POST', 'GET'], '/onetomore', ['name' => 'login', 'uses' => 'LoginController@login']);
+//});
+//Route::match(['POST', 'GET'], '/login', ['name' => 'login', 'uses' => 'LoginController@login']);
 
