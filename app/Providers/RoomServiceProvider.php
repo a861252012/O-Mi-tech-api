18fcec5a2563ecd38f38681f5af6387285ec315c
raby <raby@moonjoy.net>
2018-03-15 15:28:13 +0700

    add event

---
+++ app/Providers/RoomServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use App\Services\Room\RoomService;
+use Illuminate\Support\ServiceProvider;
+
+class RoomServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //roomService
+        $this->app->singleton('roomService', function () {
+            return new RoomService();
+        });
+    }
+}
