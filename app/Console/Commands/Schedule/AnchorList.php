f1bf343886d14586439aec6859ffd916e9217b14
nicholas <nicholas@moonjoy.net>
2018-03-27 09:03:30 +0700

    主播列表定时任务支持多站点

--- app/Console/Commands/Schedule/AnchorList.php
+++ app/Console/Commands/Schedule/AnchorList.php
@@ -3,10 +3,8 @@
 namespace App\Console\Commands\Schedule;
 
 use App\Services\Site\SiteService;
-use Illuminate\Cache\FileStore;
 use Illuminate\Console\Command;
-use Illuminate\Filesystem\Filesystem;
-use Illuminate\Http\File;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Storage;
 
@@ -48,26 +46,38 @@ class AnchorList extends Command
      */
     public function handle()
     {
-        $flashVer =  $this->siteService->config('flash_version');
-        $this->info('flash_version'.$flashVer);
+        $this->siteService->getIDs()
+            ->each([$this, 'handleForSite']);
+    }
+
+    public function handleForSite($id)
+    {
+        $this->siteService->fromID($id);
+        if (!$this->siteService->isValid()){
+            $this->info('invalid site config for id '.$id);
+            Log::error('invalid site config  ',['id'=>$id]);
+            return;
+        }
+        $flashVer = $this->siteService->config('flash_version');
+        $this->info('flash_version:' . $flashVer);
 //home_all_,home_rec_,home_ord_,home_gen_,home_vip_
-        $conf_arr = array(
-            'home_all_'=> array('所有主播','all'),
-            'home_rec_'=> array( '小编推荐','rec'),
-            'home_ord_'=> array('一对一房间','ord'),
-            'home_gen_'=> array('才艺主播','gen'),
+        $conf_arr = [
+            'home_all_' => ['所有主播', 'all'],
+            'home_rec_' => ['小编推荐', 'rec'],
+            'home_ord_' => ['一对一房间', 'ord'],
+            'home_gen_' => ['才艺主播', 'gen'],
             //'home_vip_'=> array('会员专区','vip'),
-            'home_mobile_'=> array('手机直播','mobile'),
-        );
+            'home_mobile_' => ['手机直播', 'mobile'],
+        ];
 //$json = '{';
-        foreach( $conf_arr as $key=>$item ){
-            $data =  Redis::get($key.$flashVer);
-            if( $data == null ){
-                echo $item[0].'可能出问题了，请联系java开发人员'.PHP_EOL;
-                Storage::disk('public')->put('videolist'.$item[1].'.json','{"rooms":[]}');
-            }else{
-                $data = str_replace(array('cb(',');'),array('',''),$data);
-                Storage::disk('public')->put('videolist'.$item[1].'.json',$data);
+        foreach ($conf_arr as $key => $item) {
+            $data = Redis::get($key . $flashVer);//todo 区分站点
+            if ($data == null) {
+                echo $item[0] . '可能出问题了，请联系java开发人员' . PHP_EOL;
+                Storage::disk('public')->put($this->siteService->getPublicPath() . '/videolist' . $item[1] . '.json', '{"rooms":[]}');
+            } else {
+                $data = str_replace(['cb(', ');'], ['', ''], $data);
+                Storage::disk('public')->put($this->siteService->getPublicPath() . '/videolist' . $item[1] . '.json', $data);
             }
         }
     }
