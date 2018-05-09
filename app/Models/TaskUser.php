df5152869decb54a3ca374a6c17a2ed9000ceeed
desmond <desmond@moonjoy.net>
2018-05-09 08:30:10 +0700

    model引入站点id
    Signed-off-by: desmond <desmond@moonjoy.net>

--- app/Models/TaskUser.php
+++ app/Models/TaskUser.php
@@ -8,9 +8,10 @@
  
 namespace App\Models;
 
-
+use App\Traits\SiteSpecific;
 class TaskUser extends \Illuminate\Database\Eloquent\Model
 {
+    use SiteSpecific;
     protected $table = 'video_task_user';
     protected $primaryKey = 'auto_id';
     protected $guarded = ['auto_id'];
