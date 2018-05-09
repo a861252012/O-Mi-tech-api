0cb16f4b24b9f1282f7bb8aceddc7a57b3fccdbb
desmond <desmond@moonjoy.net>
2018-05-09 08:25:29 +0700

    model引入站点id
    Signed-off-by: desmond <desmond@moonjoy.net>

--- app/Models/ActiveCommon.php
+++ app/Models/ActiveCommon.php
@@ -1,8 +1,10 @@
 <?php
 namespace App\Models;
 
+use App\Traits\SiteSpecific;
 class ActiveCommon extends \Illuminate\Database\Eloquent\Model
 {
+    use SiteSpecific;
     protected  $table='video_active_common';
     protected $primaryKey = 'activity_id';
 }
--- app/Models/CharmRank.php
+++ app/Models/CharmRank.php
@@ -1,8 +1,9 @@
 <?php
 namespace App\Models;
-
+use App\Traits\SiteSpecific;
 class CharmRank extends \Illuminate\Database\Eloquent\Model
 {
+    use SiteSpecific;
     protected  $table='video_charm_rank';
     protected $primaryKey = 'crank_id';
 
