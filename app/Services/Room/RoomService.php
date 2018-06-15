31374bd8869fbf9968ad5212e53c7d33eae7dae5
bart <bart@moonjoy.net>
2018-06-15 14:47:13 +0700

    505

--- app/Services/Room/RoomService.php
+++ app/Services/Room/RoomService.php
@@ -10,6 +10,7 @@ use App\Services\User\UserService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Session;
 use App\Facades\SiteSer;
