16f609d0a0c378fc2b189d0f5bef6af3cc5a1bdb
raby <raby@moonjoy.net>
2018-06-17 21:22:33 +0700

    计划任务错误

--- app/Console/Commands/Schedule/AnchorSearch.php
+++ app/Console/Commands/Schedule/AnchorSearch.php
@@ -88,47 +88,48 @@ class AnchorSearch extends Command
             }
         }
 
-        function make_comparer()
-        {
-            // Normalize criteria up front so that the comparer finds everything tidy
-            $criteria = func_get_args();
-            foreach ($criteria as $index => $criterion) {
-                $criteria[$index] = is_array($criterion)
-                    ? array_pad($criterion, 3, null)
-                    : array($criterion, SORT_ASC, null);
-            }
-
-            return function ($first, $second) use (&$criteria) {
-                foreach ($criteria as $criterion) {
-                    // How will we compare this round?
-                    list($column, $sortOrder, $projection) = $criterion;
-                    $sortOrder = $sortOrder === SORT_DESC ? -1 : 1;
-
-                    // If a projection was defined project the values now
-                    if ($projection) {
-                        $lhs = call_user_func($projection, $first[$column]);
-                        $rhs = call_user_func($projection, $second[$column]);
-                    } else {
-                        $lhs = $first[$column];
-                        $rhs = $second[$column];
-                    }
-
-                    // Do the actual comparison; do not return if equal
-                    if ($lhs < $rhs) {
-                        return -1 * $sortOrder;
-                    } else if ($lhs > $rhs) {
-                        return 1 * $sortOrder;
-                    }
-                }
-
-                return 0; // tiebreakers exhausted, so $first == $second
-            };
-        }
         
         if(isset($myfav_arr) && is_array($myfav_arr)){
-            usort($myfav_arr, make_comparer(['live_status', SORT_DESC], ['attens', SORT_DESC], ['lv_exp', SORT_DESC]));
+            usort($myfav_arr, $this->make_comparer(['live_status', SORT_DESC], ['attens', SORT_DESC], ['lv_exp', SORT_DESC]));
             Storage::put('cache/anchor-search-data.php','<?php ' . PHP_EOL . 'return ' . preg_replace('/[\s' . PHP_EOL . ']+/m', '', var_export($myfav_arr, true)) . ';');
         }
 
+    }
+
+    public function make_comparer()
+    {
+        // Normalize criteria up front so that the comparer finds everything tidy
+        $criteria = func_get_args();
+        foreach ($criteria as $index => $criterion) {
+            $criteria[$index] = is_array($criterion)
+                ? array_pad($criterion, 3, null)
+                : array($criterion, SORT_ASC, null);
+        }
+
+        return function ($first, $second) use (&$criteria) {
+            foreach ($criteria as $criterion) {
+                // How will we compare this round?
+                list($column, $sortOrder, $projection) = $criterion;
+                $sortOrder = $sortOrder === SORT_DESC ? -1 : 1;
+
+                // If a projection was defined project the values now
+                if ($projection) {
+                    $lhs = call_user_func($projection, $first[$column]);
+                    $rhs = call_user_func($projection, $second[$column]);
+                } else {
+                    $lhs = $first[$column];
+                    $rhs = $second[$column];
+                }
+
+                // Do the actual comparison; do not return if equal
+                if ($lhs < $rhs) {
+                    return -1 * $sortOrder;
+                } else if ($lhs > $rhs) {
+                    return 1 * $sortOrder;
+                }
+            }
+
+            return 0; // tiebreakers exhausted, so $first == $second
+        };
     }
 }
