7d42516c2bb7e15e14d0970b255045757304ff5b
raby <raby@moonjoy.net>
2018-06-20 19:34:53 +0700

    银行卡查询问题

--- app/Models/PayAccount.php
+++ app/Models/PayAccount.php
@@ -1,8 +1,11 @@
 <?php
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class PayAccount extends \Illuminate\Database\Eloquent\Model
 {
+    use SoftDeletes;
     protected  $table='video_pay_account';
     protected $primaryKey = 'id';
 }
