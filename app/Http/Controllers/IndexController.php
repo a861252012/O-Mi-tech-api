1c98e815c669ed07ad564ce9f60bb5c576a82906
yeoman <yeoman@moonjoy.net>
2018-07-12 11:13:22 +0700

    modify json-obj to array

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -273,19 +273,26 @@ class IndexController extends Controller
             unset($oneToMore);
         }
 
+        $data = [
+            'status' => 1,
+            'myfav' => $myfav,
+            'myres' => $myres,
+            'myticket' => $myticket,
+            'notice' => $notice,
+            'qqurl' => is_null($qq_url) ? '' : $qq_url,
+            'emailurl' => is_null($email_url) ? '' : $email_url,
+
+        ];
+        $data = array_map(function ($item) {
+            if (\is_array($item)) {
+                return array_values($item);
+            }
+            return $item;
+        }, $data);
+
         return JsonResponse::create(
             [
-                'data' =>
-                    [
-                        'status' => 1,
-                        'myfav' => $myfav,
-                        'myres' => $myres,
-                        'myticket' => $myticket,
-                        'notice' => $notice,
-                        'qqurl' => is_null($qq_url) ? '' : $qq_url,
-                        'emailurl' => is_null($email_url) ? '' : $email_url,
-
-                    ],
+                'data' =>$data,
                 'msg' => '获取成功',
                 'status' => 1,
             ]
