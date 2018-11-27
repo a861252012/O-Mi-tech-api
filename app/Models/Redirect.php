71f841786b13f2c6a4409da37d4fe3fdd53ccf3b
Raby <raby@moonjoy.net>
2018-11-27 09:55:47 +0000

    优化

--- app/Models/Redirect.php
+++ app/Models/Redirect.php
@@ -1,8 +1,11 @@
 <?php
 namespace App\Models;
 
+use App\Traits\SiteSpecific;
 class Redirect extends \Illuminate\Database\Eloquent\Model
 {
+    //2018-11-27： 经领导clark确认，需求作变更： 变更为代理区分一二站
+    use SiteSpecific;
     protected $table='video_redirect';
     protected $primaryKey = 'id';
     public $timestamps= false;
