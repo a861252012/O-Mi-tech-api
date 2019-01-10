dc6bb2cacbdc1650d18db35f35a5ae1570a7bee9
desmond <desmond@moonjoy.net>
2019-01-10 17:29:03 +0800

    修改手机端拉取socket值

--- app/Services/Room/SocketService.php
+++ app/Services/Room/SocketService.php
@@ -44,11 +44,12 @@ class SocketService extends Service
 //                $channelIDs->push($channelID);
                 $channelInfo = $redis->hgetall('channel_info:' . $channelID);
                 if ($this->lessLoad($channelInfo, $minLoadChannel)) {
+                    $minLoadChannel = $channelInfo;
                     $chanhost = explode('|',$channelInfo['host']);
-                        if(!empty($chanhost[0])){
+                    if(!empty($chanhost[0])){
                         $minLoadChannel['host'] = $chanhost[0];
                     }else{
-                         $minLoadChannel['host'] = $chanhost[1];
+                        $minLoadChannel['host'] = $chanhost[1];
                     }
 
                 }
