d7742136f0dbabba2572e820b119292ffb71ee3d
Stanly.liu <Stanly.liu@LGroup.global>
2019-04-17 16:25:23 +0800

    添加參數:username

--- app/Services/User/UserService.php
+++ app/Services/User/UserService.php
@@ -504,6 +504,7 @@ class UserService extends Service
                 $items->push([
                     'headimg' => $user->headimg,
                     'rid' => $user->uid,
+                    'username' => $user->username,
                     'nickname' => $user->nickname,
                     'roled' => $user->roled,
                     'lv_exp' => $user->lv_exp,
