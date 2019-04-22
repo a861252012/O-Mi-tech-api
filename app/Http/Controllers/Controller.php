ef7455c011a0179b739a551003c6e09f1f43d9b3
Stanly.liu <Stanly.liu@LGroup.global>
2019-04-22 18:03:07 +0800

    更改保级程式顺序

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1460,6 +1460,10 @@ class Controller extends BaseController
                 $userObj->vip_end = date('Y-m-d H:i:s', $newTime);
                 $userObj->save();
 
+                DB::commit();
+                // 更新完刷新redis
+                Redis::hset('huser_info:' . $user['uid'], 'vip_end', date('Y-m-d H:i:s', $newTime));
+
                 //发送私信给用户
                 VideoMail::create([
                     'send_uid' => 0,
@@ -1469,9 +1473,6 @@ class Controller extends BaseController
                     'status' => 0,
                     'created' => date('Y-m-d H:i:s'),
                 ]);
-                DB::commit();
-                // 更新完刷新redis
-                Redis::hset('huser_info:' . $user['uid'], 'vip_end', date('Y-m-d H:i:s', $newTime));
             } catch (\Exception $e) {
 //                $testPath = BASEDIR . '/app/logs/test_' . date('Y-m-d') . '.log';
                 $testInfo = "保级异常：getmypid " . getmypid() . "checkUserVipStatus 更新数据成功  \n";
