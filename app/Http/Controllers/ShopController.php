d7626fae6a0a5a812437bc12a432dc4c5238c51b
Stanly.liu <Stanly.liu@LGroup.global>
2019-06-27 21:11:05 +0800

    395 貴族清單缺少欄位

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -4,6 +4,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Goods;
+use App\Models\UserGroupPermission;
 use App\Services\UserGroup\UserGroupService;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\JsonResponse;
@@ -105,9 +106,150 @@ class ShopController extends Controller
             return (new JsonResponse($msg));
         }
 
-        $msg['data'] = $userGroup;
+        $A_userGroup = array();
+        foreach ($userGroup as $S_userGroup) {
+            $permission = UserGroupPermission::where('gid', $S_userGroup['gid'])->get()->toArray();
+            if (!empty($permission)) {
+                $permission = $this->setHtmlText($permission[0]);
+            }
+            $S_userGroup->haswelcome = $permission['haswelcome'];
+            $S_userGroup->haschateffect = $permission['haschateffect'];
+            $S_userGroup->hasvipseat = $permission['hasvipseat'];
+            array_push($A_userGroup,$S_userGroup);
+        }
+
+        $msg['data'] = $A_userGroup;
 
         return new JsonResponse($msg);
+    }
+
+    private function setHtmlText($array = array())
+    {
+        //是否限制访问房间
+        if ($array['allowvisitroom']) {
+            $data['allowvisitroom'] = '限制';
+        } else {
+            $data['allowvisitroom'] = '不限制';
+        }
+        //是否允许修改昵称
+        if ($array['modnickname'] > 0) {
+            $mod = explode('|', $array['modnickname']);
+            $data['modnickname'] = $mod[0] . '次/';
+            switch ($mod[1]) {
+                case 'week':
+                    $data['modnickname'] .= '周';
+                    break;
+                case 'month':
+                    $data['modnickname'] .= '月';
+                    break;
+                case 'year':
+                    $data['modnickname'] .= '年';
+                    break;
+            }
+        } elseif ($array['modnickname'] == 0) {
+            $data['modnickname'] = "限制";
+        } elseif ($array['modnickname'] < 0) {
+            $data['modnickname'] = "不受限";
+        }
+        //是否有进房欢迎语
+        if ($array['haswelcome']) {
+            $data['haswelcome'] = '有';
+        } else {
+            $data['haswelcome'] = '无';
+        }
+        //是否有聊天特效
+        if ($array['haschateffect']) {
+            $data['haschateffect'] = '有';
+        } else {
+            $data['haschateffect'] = '无';
+        }
+        //聊天文字长度限制
+//        $limit = explode('|',$array['chatlimit']);
+//        if(count($limit)>1){
+//            $data['chatlimit'] = $limit[0].'s/'. $limit[1];
+//        }else{
+//            $data['chatlimit'] = $limit[0];
+//        }
+        //是否拥有贵宾席
+        if ($array['hasvipseat']) {
+            $data['hasvipseat'] = '有';
+        } else {
+            $data['hasvipseat'] = '无';
+        }
+
+        //是否防止被禁言
+        if (!empty($array['nochat'])) {
+            $nochat = explode('|', $array['nochat']);
+            $data['nochat'] = '防';
+            if ($nochat[0] == 1) {
+                $data['nochat'] = '房主';
+            }
+            if ($nochat[0] == 2) {
+                $data['nochat'] = '管理员';
+            }
+            if (isset($nochat[1]) && $nochat[1] == 2) {
+                $data['nochat'] .= '、管理员';
+            }
+            if (isset($nochat[1]) && $nochat[1] == 1) {
+                $data['nochat'] .= '、房主';
+            }
+        } else {
+            $data['nochat'] = "无";
+        }
+        $data['chatlimit'] = $array['chatlimit'] . '字';
+        if ($array['chatsecond'] == 0) {
+            $data['chatsecond'] = '不受限';
+        } elseif ($array['chatsecond'] > 0) {
+            $data['chatsecond'] = $array['chatsecond'] . '秒';
+        } else {
+            $data['chatsecond'] = '肯定是游客';
+        }
+
+        //提普通用户权限
+        if ($array['nochatlimit']) {
+            $data['nochatlimit'] = $array['nochatlimit'] . '人/天';
+        } else {
+            $data['nochatlimit'] = "无";
+        }
+
+        //是否防止被踢
+        if ($array['avoidout']) {
+            $avoidout = explode('|', $array['avoidout']);
+            $data['avoidout'] = '防';
+            if ($avoidout[0] == 1) {
+                $data['avoidout'] = '房主';
+            }
+            if ($avoidout[0] == 2) {
+                $data['avoidout'] = '管理员';
+            }
+            if (isset($avoidout[1]) && $avoidout[1] == 2) {
+                $data['avoidout'] .= '、管理员';
+            }
+            if (isset($avoidout[1]) && $avoidout[1] == 1) {
+                $data['avoidout'] .= '、房主';
+            }
+        } else {
+            $data['avoidout'] = "不防";
+        }
+
+        //提普通用户权限
+        if ($array['letout']) {
+            $data['letout'] = $array['letout'] . '人/天';
+        } else {
+            $data['letout'] = "无";
+        }
+
+        //是否拥有隐身权限
+        if ($array['allowstealth']) {
+            $data['allowstealth'] = '有';
+        } else {
+            $data['allowstealth'] = "无";
+        }
+        if ($array['discount']) {
+//            $data['discount'] = $this->config['discountType'][$array['discount']];
+            $data['discount'] = $this->discountType[$array['discount']];
+        }
+        return $data;
     }
 
 }
