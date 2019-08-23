eefbb821502cdc51c6da843e48856541cb5fb9ba
Hunter Wu <hunter.wu@ileopard.com>
2019-08-23 17:36:23 +0800

    T#492 Add reg block message

--- app/Http/Controllers/RegController.php
+++ app/Http/Controllers/RegController.php
@@ -15,12 +15,15 @@ class RegController extends Controller
         $status = $regService->status();
 
         $resp = [
-            'status' => 1, 
+            'status' => 1,
             'data' => [
                 'nickname' => $randomNickname,
                 'reg_status' => $status,
             ],
-        ];        
+        ];
+        if ($status === RegService::STATUS_BLOCK) {
+            $resp['data']['msg'] = '来自相同 IP 的注册数量过多，已暂停注册功能，请联系客服处理。';
+        }
         return new JsonResponse($resp);
     }
 }
