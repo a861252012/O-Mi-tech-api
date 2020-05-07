2269b3f282cbc218d13d8f0c567d70147f4748f5
Weine <weine.f@pppop.com>
2020-05-07 17:25:10 +0900

    T#998 調整驗證碼

--- app/Services/LoginService.php
+++ app/Services/LoginService.php
@@ -8,12 +8,19 @@
 namespace App\Services;
 
 
+use App\Facades\SiteSer;
+use Illuminate\Support\Facades\Hash;
+
 class LoginService
 {
     /* 自動化驗證碼檢查 */
     public function autoCheck($captcha)
     {
-        if ($captcha == '123456') {
+//        $captchaCode = SiteSer::globalSiteConfig('auto_captcha');
+        /* 本以不分站點設定存放，討論後先以hardcode方式 */
+        $autoCaptcha = 'e10adc3949ba59abbe56e057f20f883e';
+
+        if ($captcha == $autoCaptcha) {
             return true;
         }
 
