24c82056785350c4303e90e8150201416a57e3cf
hunter.w <hunter.w@lgroup.global>
2020-06-16 16:13:07 +0800

    T#1131 新增重置首充欄位命令

--- app/Console/Commands/Tools.php
+++ app/Console/Commands/Tools.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Users;
 use App\Services\User\UserService;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Redis;
@@ -51,6 +52,52 @@ class Tools extends Command
         echo 'Method not exists!';
     }
 
+    // 針對未達一富的用戶，清空其首充欄位
+    private function resetFirstChargeInSixMonth()
+    {
+        $afterTs = time() - self::SIX_MONTH_IN_SEC;
+        $this->resetFirstChargeAfter($afterTs);
+    }
+    private function resetFirstChargeTest()
+    {
+        $afterTs = time() - 86400*20;
+        $this->resetFirstChargeAfter($afterTs);
+    }
+    private function resetFirstChargeAfter($afterTs = 0)
+    {
+        if ($afterTs == 0) {
+            $afterTs = time(); // default: do nothing
+        }
+
+        $userService = resolve(UserService::class);
+        $cursor = $now = time();
+        while ($cursor > $afterTs) {
+            $cnt = 0;
+            $data= Users::query()
+                ->where('created', '<', date('Y-m-d H:i:s', $now))
+                ->where('created', '>', date('Y-m-d H:i:s', $cursor))
+                ->where('rich', '<', 500)
+                ->whereNotNull('first_charge_time')
+                ->get();
+
+            foreach ($data as $user) {
+                $uid = $user->uid;
+
+                // update
+                Users::where('uid', $uid)->update(['first_charge_time' => null]);
+
+                // delete cache
+                $userService->cacheUserInfo($uid, null);
+                $cnt++;
+            }
+            if ($cnt > 0) {
+                echo date('Y-m-d H:i:s:', $cursor), $cnt, "\n";
+            }
+            $cursor -= 86400;
+        }
+    }
+
+    // 刪除過舊 sid
     private function removeSidSixMonthOld()
     {
         $this->scanHUserSid(self::SIX_MONTH_IN_SEC);
@@ -63,7 +110,6 @@ class Tools extends Command
     {
         $this->scanHUserSid(self::TWO_MONTH_IN_SEC);
     }
-
     private function scanHUserSid($diff = self::SIX_MONTH_IN_SEC)
     {
         $userService = resolve(UserService::class);
