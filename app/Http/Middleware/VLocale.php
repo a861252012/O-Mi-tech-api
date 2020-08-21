d9ce98f64f1375c56cc431b7ea9ba190d70f58f7
Weine <weine.f@pppoop.com>
2020-08-21 19:11:44 +0900

    調整多國語底層判斷與預設邏輯

--- app/Http/Middleware/VLocale.php
+++ app/Http/Middleware/VLocale.php
@@ -11,6 +11,8 @@ use App\Services\UserAttrService;
 use Closure;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use function Psy\debug;
 
 class VLocale
 {
@@ -29,27 +31,36 @@ class VLocale
         $userAttrService = resolve(UserAttrService::class);
         $userLocale = $userAttrService->get($uid, 'locale');
 
+        $locale = 'zh';
         if (!empty($userLocale)) {
             $locale = $userLocale;
         } elseif (!empty($request->query('locale'))) {
-            $localeArr = explode('_', $request->query('locale'));
+            if (str_contains($request->query('locale'), '-')) {
+                $localeArr = explode('-', $request->query('locale'));
+            }
 
             if (in_array($request->query('locale'), self::LANGS)) {
                 $locale = $request->query('locale');
+            } elseif (in_array($localeArr[0] . '_' . $localeArr[1], self::LANGS)) {
+                $locale = $localeArr[0] . '_' . $localeArr[1];
             } elseif (in_array($localeArr[0], self::LANGS)) {
                 $locale = $localeArr[0];
             }
         } elseif (!empty($request->header('Accept-Language'))) {
-            $localeArr = explode('_', $request->header('Accept-Language'));
+            if (str_contains($request->header('Accept-Language'), '-')) {
+                $localeArr = explode('-', $request->header('Accept-Language'));
+            }
 
             if (in_array($request->header('Accept-Language'), self::LANGS)) {
                 $locale = $request->header('Accept-Language');
+            } elseif (in_array($localeArr[0] . '_' . $localeArr[1], self::LANGS)) {
+                $locale = $localeArr[0] . '_' . $localeArr[1];
             } elseif (in_array($localeArr[0], self::LANGS)) {
                 $locale = $localeArr[0];
             }
-        } else {
-            $locale = 'zh';
         }
+
+        Log::debug('用戶request語系: ' . $locale);
 
         App::setLocale($locale);
 
