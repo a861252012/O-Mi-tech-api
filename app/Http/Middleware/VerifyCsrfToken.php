400e3997382a22d878a1e68b13055088b0765880
hunter.w <hunter.w@lgroup.global>
2020-11-26 11:53:12 +0800

    T#1523 Add use TokenMismatchException

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -6,6 +6,7 @@ use Closure;
 use App\Facades\SiteSer;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Session\TokenMismatchException;
 
 class VerifyCsrfToken extends Middleware
 {
